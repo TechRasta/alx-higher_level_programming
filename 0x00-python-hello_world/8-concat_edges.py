1abaed8ba2ed4208d65c47257ebeb10778cd8c85
TheoCaesar <theocaesar7@gmail.com>
2022-11-30 13:41:14 -0800

    edit

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print("{}".format(str))
+str = str[39:6] + str[10:112] + str[:6]
+print(str)
